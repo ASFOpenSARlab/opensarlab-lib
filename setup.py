21cbfba7e1170c201d3f433e05981f1c26899012
Alex-Lewandowski <aflewandowski@alaska.edu>
2021-07-12 00:23:30 -0800

    setuptools_scm[toml]>=3.4

--- setup.py
+++ setup.py
@@ -34,24 +34,6 @@ setup(
     python_requires='~=3.8',
 
     install_requires=[
-        'asf_hyp3',
-        'datetime',
-        'gdal',
-        'getpass',
-        'hyp3_sdk',
-        'IPython',
-        'ipywidgets',
-        'matplotlib',
-        'matplotlib.pyplot',
-        'matplotlib.widgets',
-        'numpy',
-        'osgeo',
-        'pandas',
-        'requests',
-        'subprocess',
-        'sys',
-        'typing',
-        'zipfile',
     ],
 
     extras_require={
