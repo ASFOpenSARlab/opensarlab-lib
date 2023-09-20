d0f3fc4d3388caf46b6cebe7f30a42cb06fda039
Dylan Palmieri <dgpalmieri@gmail.com>
2023-09-20 11:03:21 -0800

    fix: add cartopy to install_requires style: apply `black` formatting

--- setup.py
+++ setup.py
@@ -3,53 +3,50 @@ import os
 from setuptools import find_packages, setup
 
 _HERE = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(_HERE, 'README.md'), 'r') as f:
+with open(os.path.join(_HERE, "README.md"), "r") as f:
     long_desc = f.read()
 
 setup(
-    name='opensarlab_lib',
+    name="opensarlab_lib",
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
     description="A Python library to support ASF OpenSARlab's Jupyter Notebook repository https://github.com/ASFOpenSARlab/opensarlab-notebooks",
     long_description=long_desc,
-    long_description_content_type='text/markdown',
-
-    url='https://github.com/ASFOpenSARlab/opensarlab-lib',
-
-    author='ASF OpenSARlab Team',
-    author_email='uaf-jupyterhub-asf@alaska.edu',
-
-    license='BSD',
+    long_description_content_type="text/markdown",
+    url="https://github.com/ASFOpenSARlab/opensarlab-lib",
+    author="ASF OpenSARlab Team",
+    author_email="uaf-jupyterhub-asf@alaska.edu",
+    license="BSD",
     include_package_data=True,
-
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
-
     packages=find_packages(),
-    python_requires='>=3.8',
-
+    python_requires=">=3.8",
     install_requires=[
-        'numpy',
-        'pyproj',
-        'asf_search',
-        'hyp3_sdk',
-        'IPython',
-        'ipywidgets',
-        'matplotlib',
-        'pandas',
-        'pytest',
-        'pytest-cov',
-        'requests',
+        "asf_search",
+        "cartopy",
+        "gdal",
+        "hyp3_sdk",
+        "ipywidgets",
+        "IPython",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "pyproj",
+        "requests",
     ],
-
     extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+        ]
     },
 )
