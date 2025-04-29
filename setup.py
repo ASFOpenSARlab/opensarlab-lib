8b571756b8b2a0236a487e4bd3a1ce840886bb0e
Alex-Lewandowski <aflewandowski@alaska.edu>
2025-04-29 12:06:36 -0800

    switch from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,53 +1,5 @@
-import os
+# setup.py (optional stub for backward compatibility)
+# Now using pyproject.toml
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-_HERE = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(_HERE, "README.md"), "r") as f:
-    long_desc = f.read()
-
-setup(
-    name="opensarlab_lib",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    description="A Python library to support ASF OpenSARlab's Jupyter Notebook repository https://github.com/ASFOpenSARlab/opensarlab-notebooks",
-    long_description=long_desc,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ASFOpenSARlab/opensarlab-lib",
-    author="ASF OpenSARlab Team",
-    author_email="uaf-jupyterhub-asf@alaska.edu",
-    license="BSD",
-    include_package_data=True,
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    packages=find_packages(),
-    python_requires=">=3.8",
-    install_requires=[
-        "asf_search",
-        "cartopy",
-        "contextily",
-        "gdal",
-        "hyp3_sdk",
-        "ipywidgets",
-        "IPython",
-        "matplotlib",
-        "numpy",
-        "pandas",
-        "pyproj",
-        "requests",
-    ],
-    extras_require={
-        "dev": [
-            "pytest",
-            "pytest-cov",
-        ]
-    },
-)
+setup()
